dab62dab292261ea42bb63816098dac70610becf
King Irrisoire <iluminadopelalua@gmail.com>
2024-05-26 00:16:00 -0300

    fiz a logica da barra de pesquisa

---
+++ Controllers/MangaController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using scan.Context;
+using scan.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace scan.Controllers
+{
+    public class MangaController : Controller
+    {
+        private readonly IronicusScanContext _context;
+
+        public MangaController(IronicusScanContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult SearchResults(string searchTerm)
+        {
+            List<Manga> results = new List<Manga>();
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                results = _context.Mangas
+                    .Where(m => m.Title.Contains(searchTerm))
+                    .ToList();
+            }
+
+            return View(results);
+        }
+    }
+}
